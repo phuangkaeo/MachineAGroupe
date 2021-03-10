63f555692868bdb29547f065ed2baa9c62b23421
phuangkaeo <phichet.phuangkaeo@free.com>
2021-03-10 13:53:32 +0100

    The second version uploaded

--- main.py
+++ main.py
@@ -81,65 +81,58 @@ def create_grp():
 
             """Methode qui permet de tester une partie du code """
 
-            @autotest
-            def calcul(nb_part):
+@autotest
+def calcul(nb_part):
+    """NB only INT
 
-                """NB only INT
+    >>> calcul(4)
+    4
 
-                >>> calcul(4)
-                4
-                """
-                return 16 // int(nb_part)
+    """
+    return 16 // int(nb_part)
 
-            """methode qui vérouille la demande en interger et limite les demandes de groupe de 1 à 8 participants maximum"""
+    """methode qui vérouille la demande en interger et limite les demandes de groupe de 1 à 8 participants maximum"""
 
-            def check_nb_part():
 
-                nb = "0"
-                while int(nb) > 8 or int(nb) == 0:
+def check_nb_part():
+    nb = "0"
+    while int(nb) > 8 or int(nb) == 0:
+        try:
+            nb = int(input("\033[32mVeuillez entrer le nombre de personne dans un groupe entre 1 et 8 personnes: \033[0m"))
+        except ValueError:\
+                print("Vous n'avez pas saisi de nombre entre 1 et 8")
 
-                    try:
-                        nb = int(input(
-                            "\033[32mVeuillez entrer le nombre de personne dans un groupe entre 1 et 8 personnes: \033[0m"))
+        nb_part = nb
 
-                    except ValueError:
-                        print("Vous n'avez pas saisi de nombre entre 1 et 8")
+        return nb_part
 
-                nb_part = nb
+import random
 
-                return nb_part
+print("\033[32m\n----------------------------------------\n\033[0m")
+print('\033[32mBienvenue dans votre application.\033[0m')
+print("\033[32m\n----------------------------------------\n\033[0m")
 
-            import random
+"""boucle qui indique à l'utilisateur ce qu'il souhaite faire et lance les méthodes en fonction des réponses"""
+choix = ""
+while choix != "q":
+    choix = input("\033[33m\n(s) Pour créer des groupes.\n(q) Pour quitter.\nVotre choix :\033[0m").lower()
+    if choix == "s":
+        participant = ["Farid", "Marie", "Phichet", "Arthur", "Antoine", "Hatice", "Giovanni", "Mickael",
+                       "Rachid", "Julien", "Vivien", "Kevin", "Josephine", "Valentin", "Camille", "Tanguy"]
+        nb_part = check_nb_part()
+        nb_grp = calcul(nb_part)
+        x = range(int(nb_part))
+        y = range(int(nb_grp))
+        list_grp = []
 
-            print("\033[32m\n----------------------------------------\n\033[0m")
-            print('\033[32mBienvenue dans votre application.\033[0m')
-            print("\033[32m\n----------------------------------------\n\033[0m")
-
-            """boucle qui indique à l'utilisateur ce qu'il souhaite faire et lance les méthodes en fonction des réponses"""
-            choix = ""
-            while choix != "q":
-                choix = input("\033[33m\n(s) Pour créer des groupes.\n(q) Pour quitter.\nVotre choix :\033[0m").lower()
-                if choix == "s":
-                    participant = ["Farid", "Marie", "Phichet", "Arthur", "Antoine", "Hatice", "Giovanni", "Mickael",
-                                   "Rachid", "Julien", "Vivien", "Kevin", "Josephine", "Valentin", "Camille", "Tanguy"]
-
-                    nb_part = check_nb_part()
-
-                    nb_grp = calcul(nb_part)
-                    x = range(int(nb_part))
-                    y = range(int(nb_grp))
-                    list_grp = []
-
-                    A = ""
-                    lvl = ""
-                    while A != "o" and A != "n":
-                        A = input("\033[32m Souhaitez-vous faire des groupes par compétence. (o/n) : \033[0m").lower()
-
-                        if A == "n":
-                            create_grp()
-
-                        if A == "o":
-                            comp(lvl, comp)
+        A = ""
+        lvl = ""
+        while A != "o" and A != "n":
+            A = input("\033[32m Souhaitez-vous faire des groupes par compétence. (o/n) : \033[0m").lower()
+            if A == "n":
+                create_grp()
+                if A == "o":
+                    comp(lvl, comp)
 
                 if choix == "q":
                     print("A bientôt.")
